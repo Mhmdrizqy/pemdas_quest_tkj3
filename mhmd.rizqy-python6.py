ee403f836f80229428099446aa5962bb1c6ce455
Mhmdrizqy <112367853+Mhmdrizqy@users.noreply.github.com>
2022-09-05 12:13:57 +0700

    mriski python 6
    tugas pemdas python 6

---
+++ mhmd.rizqy-python6.py
@@ -0,0 +1,15 @@
+# dolar ke rupiah
+dolar = int(input("masukan nilai dolar"))
+nilai_r = dolar * 14920
+print("nillai 1 dolar adalah ", nilai_r)
+
+# euro ke won
+
+euro = int(input("masukan nilai euro; "))
+nilai_w = euro * 1357
+print("nilai i euro adalah", nilai_w)
+
+# yen ke rupiah
+yen =int(input("masukan nilai yen;"))
+nilai_r = yen * 106
+print("nilai 1 yen adalah",nilai_r)
